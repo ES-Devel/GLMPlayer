4d04f200c797fcc4f4393020b76f1ee8163cb6dc
david <william.parras.mendez@gmail.com>
2012-11-26 00:04:41 -0600

    nuevos archivos

---
+++ glmplayer.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import wx
+import util
+import application
+from ctypes import *
+import sys
+
+'''main app'''
+def main():
+	application.GLMPlayer = wx.App()
+	application.main = util.Window(None)
+	application.main.SetTitle("GLMPlayer")
+	application.main.SetBackgroundColour( wx.Colour( 0, 0, 0))
+	application.main.SetMenuBar(util.extMenuBar(application.APP_EXIT))
+	util.Toolbar(application.main)
+	application.main.Show()
+	application.main.Bind(wx.EVT_MENU,application.main.OnQuit,id=application.APP_EXIT)
+	application.GLMPlayer.MainLoop()
+if __name__=='__main__':
+	main()
+	
